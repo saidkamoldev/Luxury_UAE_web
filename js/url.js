0fde1540b59f4be356f404ae84fe04b520d44b0b
saidkamoldev <juraqulovsaidkamol@gmail.com>
2024-09-09 16:19:52 +0500

    form

--- js/url.js
+++ js/url.js
@@ -74,6 +74,68 @@
 // }
 
 
+// document.addEventListener('DOMContentLoaded', function() {
+//     var button = document.querySelector('#contactForm button'); // To'g'ri ID yoki klass nomi ishlatilmoqda
+
+//     // Element mavjudligini tekshirish
+//     if (button) {
+//         button.addEventListener('click', function(event) {
+//             event.preventDefault();
+//             var name = document.querySelector('input[name="cname"]').value;
+//             var phone = document.querySelector('input[name="ctel"]').value;
+//             var message = document.querySelector('textarea[name="cmessage"]').value;
+
+//             if (!name || !phone || !message) {
+//                 console.error("Bir yoki bir nechta input elementlar topilmadi yoki qiymatlar bo'sh!");
+//                 return;
+//             }
+
+//             var data = {
+//                 name: name,
+//                 phone_number: phone,
+//                 message: message
+//             };
+
+//             sendDataToServer(data);
+//         });
+//     } else {
+//         console.error("Button element topilmadi!");
+//     }
+// });
+
+// function sendDataToServer(data) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('POST', 'http://0.0.0.0:8000/client/', true);
+//     xhr.setRequestHeader('Content-Type', 'application/json');
+//     xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken')); // CSRF token yuborish
+//     xhr.onload = function() {
+//         if (xhr.status === 200 || xhr.status === 201) {
+//             console.log('Ma\'lumot yuborildi:', xhr.responseText);
+//         } else {
+//             console.error('Xato:', xhr.status, xhr.statusText);
+//         }
+//     };
+//     xhr.send(JSON.stringify(data));
+// }
+
+// function getCookie(name) {
+//     let cookieValue = null;
+//     if (document.cookie && document.cookie !== '') {
+//         const cookies = document.cookie.split(';');
+//         for (let i = 0; i < cookies.length; i++) {
+//             const cookie = cookies[i].trim();
+//             if (cookie.substring(0, name.length + 1) === (name + '=')) {
+//                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//                 break;
+//             }
+//         }
+//     }
+//     return cookieValue;
+// }
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var button = document.querySelector('#contactForm button'); // To'g'ri ID yoki klass nomi ishlatilmoqda
 
@@ -111,9 +173,21 @@ function sendDataToServer(data) {
     xhr.onload = function() {
         if (xhr.status === 200 || xhr.status === 201) {
             console.log('Ma\'lumot yuborildi:', xhr.responseText);
+
+            // Serverdan ma'lumot yuborilganligi haqidagi javobni tekshirish
+            var response = JSON.parse(xhr.responseText);
+            if (response.status === true) {
+                // Input maydonlarini tozalash
+                document.querySelector('input[name="cname"]').value = '';
+                document.querySelector('input[name="ctel"]').value = '';
+                document.querySelector('textarea[name="cmessage"]').value = '';
+            }
         } else {
             console.error('Xato:', xhr.status, xhr.statusText);
         }
+    };
+    xhr.onerror = function() {
+        console.error('Tarmoq xatosi yuz berdi.');
     };
     xhr.send(JSON.stringify(data));
 }
@@ -132,7 +206,3 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-
-
-
-
